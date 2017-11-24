6a65cf803d1d51b81af12dc92dec7a4f02ae32b5
DevonMars <devon_marsham_16@live.nl>
2017-11-24 12:19:31 +0100

    Delete werkend

--- api/v1/shopping-list.routes.v1.js
+++ api/v1/shopping-list.routes.v1.js
@@ -4,33 +4,43 @@ var mongodb = require('../../config/mongo.db');
 var ShoppingList = require('../../model/shopping-list.model');
 
 routes.get('/shopping-list', function (req, res) {
-	res.contentType('application/json');
-	
-	ShoppingList.find({})
-		.then((shoppingList) => {
-			console.log(shoppingList);
-			res.status(200).json(shoppingList);
-		})
-		.catch((error) => res.status(401).json(error));
+    res.contentType('application/json');
+
+    ShoppingList.find({})
+        .then((shoppingList) => {
+            console.log(shoppingList);
+            res.status(200).json(shoppingList);
+        })
+        .catch((error) => res.status(401).json(error));
 });
 
-routes.get('/shopping-list/:id', function(req, res) {
-	const id = req.params.id;
-	
-	ShoppingList.find({'_id': id})
-		.then((shoppingList) => {
-			console.log(shoppingList);
-			res.status(200).json(shoppingList);
-		})
-		.catch((error) => res.status(401).json(error));
+routes.get('/shopping-list/:id', function (req, res) {
+    const id = req.params.id;
+
+    ShoppingList.find({'_id': id})
+        .then((shoppingList) => {
+            console.log(shoppingList);
+            res.status(200).json(shoppingList);
+        })
+        .catch((error) => res.status(401).json(error));
 });
 
-routes.post('/shopping-list', function(req, res) {
-	const body = req.body;
-	
-	ShoppingList.create(body)
-		.then((driver) => res.status(200).send(driver))
-		.catch((error) => res.status(401).json(error));
-});
+routes.delete('/shopping-list/:id', function (req, res) {
+    const id = req.params.id;
 
+    // ShoppingList.findByIdAndRemove({'_id': id})
+    //     .then((shoppingList) => res.status(200).send(shoppingList))
+    //     .catch((error) => res.status(401).json(error));
+
+    ShoppingList.findByIdAndRemove({'_id': id})
+        .then((shoppingList) => {
+            if (shoppingList == null || shoppingList == 'null') {
+                res.status(200).json({error: 'No objects deleted.'});
+            } else {
+                res.status(200).send(shoppingList);
+            }
+        })
+        .catch((error) => res.status(401).json(error));
+
+});
 module.exports = routes;
