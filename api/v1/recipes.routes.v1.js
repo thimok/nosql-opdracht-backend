6cfb908d989cee3a3248ad37f247c2983af25b01
Twan van Maastricht <ajmaastr@avans.nl>
2017-11-24 11:35:31 +0100

    Basic structure for Recipes CRUD

--- api/v1/recipes.routes.v1.js
+++ api/v1/recipes.routes.v1.js
@@ -1,3 +1,22 @@
 /**
  * Created by twanv on 24-11-2017.
  */
+var express = require('express');
+var routes = express.Router();
+var Recipe = require('../../model/recipe.model');
+
+routes.get('/recipes', function(req, res) {
+
+});
+
+routes.post('/recipes', function(req, res) {
+
+});
+
+routes.put('/recipes/:id', function(req, res) {
+
+});
+
+routes.delete('/recipes/:id', function(req, res) {
+
+});
