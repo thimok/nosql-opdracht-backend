796d170046f63c4e071a9dd217219ff067a77f10
lukamarijn <lukabrinkmannn@gmail.com>
2017-11-27 15:58:53 +0100

    Fixessss

--- api/v1/shopping-list.routes.v1.js
+++ api/v1/shopping-list.routes.v1.js
@@ -43,8 +43,13 @@ routes.put('/shopping-list/:id', function (req, res) {
 
 routes.post('/shopping-list', function (req, res) {
 	const body = req.body;
-	
-	ShoppingList.create(body)
+
+	const shoppingListProps = {
+		name: body.name,
+		amount: body.amount
+	};
+
+	ShoppingList.create(shoppingListProps)
 		.then((driver) => res.status(200).send(driver))
 		.catch((error) => res.status(400).json(error));
 });
